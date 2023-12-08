1bb43711e8468eb301399110ac122d709ee5c3f9
mhemd masarwe <mhemd.masa@gmail.com>
2023-12-08 17:36:48 +0200

    commit for modification

--- Project_2_ Visual Calculator/scripts/camera_interface.py
+++ Project_2_ Visual Calculator/scripts/camera_interface.py
@@ -2,13 +2,13 @@ import cv2
 
 
 def main():
-    video_feed - cv2.VideoCapture(0)
+    video_feed = cv2.VideoCapture(0)
 
     while(1):
         frame = video_feed.read()
         cv2.imshow("camera_feed" , frame)
-        cv.waitKey(1)
+        cv2.waitKey(1)
 
         
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
