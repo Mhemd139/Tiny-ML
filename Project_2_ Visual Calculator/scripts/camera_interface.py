6df59d564907d3f6c16c3c8c4beebf88a6b29cdb
mhemd masarwe <mhemd.masa@gmail.com>
2023-12-30 06:26:59 +0200

    update for frame index

--- Project_2_ Visual Calculator/scripts/camera_interface.py
+++ Project_2_ Visual Calculator/scripts/camera_interface.py
@@ -2,11 +2,11 @@ import cv2
 
 
 def main():
-    video_feed = cv2.VideoCapture(0)
+    video_feed = cv2.VideoCapture(1000)
 
     while(1):
         frame = video_feed.read()
-        cv2.imshow("camera_feed" , frame)
+        cv2.imshow("camera_feed" , frame[1])
         cv2.waitKey(1)
 
         
